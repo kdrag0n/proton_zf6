54feba1662a4222a840477f75256e5eebb04b469
LuK1337 <priv.luk@gmail.com>
2016-12-06 13:48:29 +0100

    input: ft8719: Return raw gesture_mode value
    Change-Id: I41ff6f76cf3cbe038110aa0b88e85a2678e0c9bc
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/input/touchscreen/focaltech_touch_ft8719/focaltech_gesture.c
+++ drivers/input/touchscreen/focaltech_touch_ft8719/focaltech_gesture.c
@@ -587,13 +587,7 @@ static ssize_t switch_gesture_mode_store(struct device *dev, struct device_attri
 
 static ssize_t switch_gesture_mode_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	bool tmp = 0;
-
-	tmp = (fts_data->gesture_mode_type >> 6) & 1;
-	if (!tmp)
-		return sprintf(buf, "%x \n", tmp);
-	else
-		return sprintf(buf, "%x \n", fts_data->gesture_mode_type);
+	return sprintf(buf, "%d\n", fts_data->gesture_mode_type);
 }
 
 /*****************************************************************************
