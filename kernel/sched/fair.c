87fb6331f5c18150a95bacb8684d614dc3637147
Quentin Perret <quentin.perret@arm.com>
2018-10-11 10:14:29 +0100

    TESTING: sched: Factor out energy calculation in a common function
    EAS can use two different energy models during wake-up. In order to ease
    profiling and comparison of the two approaches, factor all the energy
    calculation into a common function which is common entry point for both
    EM.
    
    This should be useful only for profiling.
    
    Change-Id: Ibb3ece527521bd76b4e2a631ae1fdc74bf0b6f30
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6492,6 +6492,35 @@ static void dump_eenv_debug(struct energy_env *eenv)
 static long compute_energy_simplified(struct task_struct *p, int dst_cpu,
 							struct perf_domain *pd);
 
+int calc_total_energy(struct energy_env *eenv, struct sched_domain *sd,
+		       struct perf_domain *pd)
+{
+	int cpu_idx, cpu;
+
+	if (sched_feat(EAS_SIMPLIFIED_EM) && pd) {
+		for (cpu_idx = EAS_CPU_PRV; cpu_idx < eenv->max_cpu_count; ++cpu_idx) {
+			cpu = eenv->cpu[cpu_idx].cpu_id;
+			if (cpu < 0)
+				continue;
+			eenv->cpu[cpu_idx].energy = compute_energy_simplified(eenv->p, cpu, pd);
+		}
+
+	} else {
+		struct sched_group *sg = sd->groups;
+		do {
+			/* Skip SGs which do not contains a candidate CPU */
+			if (!cpumask_intersects(&eenv->cpus_mask, sched_group_span(sg)))
+				continue;
+
+			eenv->sg_top = sg;
+			if (compute_energy(eenv) == -EINVAL)
+				return EAS_CPU_PRV;
+		} while (sg = sg->next, sg != sd->groups);
+		/* remember - eenv energy values are unscaled */
+	}
+	return 0;
+}
+
 /*
  * select_energy_cpu_idx(): estimate the energy impact of changing the
  * utilization distribution.
@@ -6523,11 +6552,11 @@ static inline int select_energy_cpu_idx(struct energy_env *eenv)
 {
 	int last_cpu_idx = eenv->max_cpu_count - 1;
 	struct sched_domain *sd;
-	struct sched_group *sg;
 	struct perf_domain *pd;
 	int sd_cpu = -1;
 	int cpu_idx;
 	int margin;
+	int ret;
 
 	sd_cpu = eenv->cpu[EAS_CPU_PRV].cpu_id;
 	sd = rcu_dereference(per_cpu(sd_ea, sd_cpu));
@@ -6543,24 +6572,11 @@ static inline int select_energy_cpu_idx(struct energy_env *eenv)
 		if (cpu < 0)
 			continue;
 		cpumask_set_cpu(cpu, &eenv->cpus_mask);
-
-		if (sched_feat(EAS_SIMPLIFIED_EM) && pd)
-			eenv->cpu[cpu_idx].energy = compute_energy_simplified(eenv->p, cpu, pd);
 	}
 
-	if (!sched_feat(EAS_SIMPLIFIED_EM)) {
-		sg = sd->groups;
-		do {
-			/* Skip SGs which do not contains a candidate CPU */
-			if (!cpumask_intersects(&eenv->cpus_mask, sched_group_span(sg)))
-				continue;
-
-			eenv->sg_top = sg;
-			if (compute_energy(eenv) == -EINVAL)
-				return EAS_CPU_PRV;
-		} while (sg = sg->next, sg != sd->groups);
-		/* remember - eenv energy values are unscaled */
-	}
+	ret = calc_total_energy(eenv, sd, pd);
+	if (ret)
+		return ret;
 
 	/*
 	 * Compute the dead-zone margin used to prevent too many task
