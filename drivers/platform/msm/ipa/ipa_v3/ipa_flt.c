70595bc7c6bfec91fce070707d4fad8aae1ed862
Yaroslav Furman <yaro330@gmail.com>
2019-05-29 18:13:11 +0300

    msm: ipa: fix a few misleading indentations
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/platform/msm/ipa/ipa_v3/ipa_flt.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_flt.c
@@ -1157,12 +1157,12 @@ int ipa3_add_flt_rule_usr(struct ipa_ioc_add_flt_rule *rules, bool user_only)
 			 */
 			if (ipa3_ctx->ipa_fltrt_not_hashable)
 				rules->rules[i].rule.hashable = false;
-				result = __ipa_add_ep_flt_rule(rules->ip,
-					rules->ep,
-					&rules->rules[i].rule,
-					rules->rules[i].at_rear,
-					&rules->rules[i].flt_rule_hdl,
-					user_only);
+			result = __ipa_add_ep_flt_rule(rules->ip,
+				rules->ep,
+				&rules->rules[i].rule,
+				rules->rules[i].at_rear,
+				&rules->rules[i].flt_rule_hdl,
+				user_only);
 			} else
 				result = -1;
 
