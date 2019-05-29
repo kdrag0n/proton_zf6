b560ebe89f9110984e395962e99dc22b15eaf134
Danny Lin <danny@kdrag0n.dev>
2019-05-28 22:11:00 -0700

    flasher: Hex-patch the kernel to preserve Magisk if installed
    Magisk performs this patch during installation to make magiskinit
    execute on every boot and inject magiskd among other other tasks. Do the
    same if Magisk is detected so that the user does not have to re-flash
    Magisk after the kernel.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- flasher/anykernel.sh
+++ flasher/anykernel.sh
@@ -31,6 +31,23 @@ dump_boot;
 
 # begin ramdisk changes
 
+decomp_image=$home/Image
+comp_image=$decomp_image.gz
+
+# Hex-patch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+# This negates the need to reflash Magisk afterwards
+if [ -f $comp_image ]; then
+  if [ -d $ramdisk/.backup ]; then
+    ui_print "  • Preserving Magisk";
+    $bin/magiskboot decompress $comp_image $decomp_image;
+    $bin/magiskboot hexpatch $decomp_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot compress=gzip $decomp_image $comp_image;
+  fi;
+
+  # Concatenate all DTBs to the kernel
+  cat $comp_image $home/dtbs/*.dtb > $comp_image-dtb;
+  rm -f $decomp_image $comp_image
+fi;
 
 # end ramdisk changes
 
