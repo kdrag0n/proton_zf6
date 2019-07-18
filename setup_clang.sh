f27855712a2adde642b67062d139615a07076d81
Danny Lin <danny@kdrag0n.dev>
2019-07-17 23:09:40 -0700

    Import kernel build and helper scripts
    Based on my WSL 2 and Pixel 3 scripts from
    https://github.com/kdrag0n/proton_bluecross and
    https://github.com/kdrag0n/proton_wsl2 respectively.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ setup_clang.sh
@@ -0,0 +1,37 @@
+#
+# Environment setup metascript for arm64 Android kernel builds with Clang
+# Copyright (C) 2019 Danny Lin <danny@kdrag0n.dev>
+#
+# This script must be *sourced* from a Bourne-compatible shell in order to
+# function. Nothing will happen if you execute it.
+#
+
+# Path to executables in Clang toolchain
+clang_bin="$HOME/toolchains/aosp-clang-9.0.3/bin"
+
+# 64-bit GCC toolchain prefix
+gcc_prefix64="$HOME/toolchains/aosp-gcc-4.9/bin/aarch64-linux-android-"
+
+# 32-bit GCC toolchain prefix
+gcc_prefix32="$HOME/toolchains/aosp-gcc32-4.9/bin/arm-linux-androideabi-"
+
+# Number of parallel jobs to run
+# Do not remove; set to 1 for no parallelism.
+jobs=$(nproc)
+
+# Do not edit below this point
+# ----------------------------
+
+# Load the shared helpers
+source helpers.sh
+
+export PATH="$clang_bin:$PATH"
+
+kmake_flags+=(
+	CC="clang"
+	CROSS_COMPILE="$gcc_prefix64"
+	CROSS_COMPILE_ARM32="$gcc_prefix32"
+	CLANG_TRIPLE="aarch64-linux-gnu-"
+
+	KBUILD_COMPILER_STRING="$(get_clang_version clang)"
+)
