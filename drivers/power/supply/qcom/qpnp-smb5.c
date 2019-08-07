0790c78c06a69a269def8834eb4e775adeeb2254
Danny Lin <danny@kdrag0n.dev>
2019-08-07 05:05:33 +0000

    power: smb5: Fix warning introduced by Asus
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -724,8 +724,9 @@ static int smb5_usb_get_prop(struct power_supply *psy,
 	struct smb_charger *chg = &chip->chg;
 	union power_supply_propval pval;
 	int rc = 0;
-	val->intval = 0;
 	u8 stat;  //Add the interface for charging debug apk
+
+	val->intval = 0;
 
 	switch (psp) {
 	case POWER_SUPPLY_PROP_PRESENT:
