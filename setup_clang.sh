fca7dd4a522dee015fd905fd2b948d585ca4f7e1
Danny Lin <danny@kdrag0n.dev>
2019-08-18 22:25:44 -0700

    setup_clang: Update to 2019/08/18 toolchain build
    https://kdrag0n.dev/files/toolchains/proton_clang-10.0.0-20190818-full.tar.zst
    On LLVM commit 2eb7951da39287ff12be9def2f0b08535c25a799
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- setup_clang.sh
+++ setup_clang.sh
@@ -7,13 +7,13 @@
 #
 
 # Path to executables in Clang toolchain
-clang_bin="$HOME/toolchains/proton_clang-10.0.0-20190806/bin"
+clang_bin="$HOME/toolchains/proton_clang-10.0.0-20190818/bin"
 
 # 64-bit GCC toolchain prefix
-gcc_prefix64="$HOME/toolchains/proton_clang-10.0.0-20190806/bin/aarch64-linux-gnu-"
+gcc_prefix64="$HOME/toolchains/proton_clang-10.0.0-20190818/bin/aarch64-linux-gnu-"
 
 # 32-bit GCC toolchain prefix
-gcc_prefix32="$HOME/toolchains/proton_clang-10.0.0-20190806/bin/arm-linux-gnueabi-"
+gcc_prefix32="$HOME/toolchains/proton_clang-10.0.0-20190818/bin/arm-linux-gnueabi-"
 
 # Number of parallel jobs to run
 # Do not remove; set to 1 for no parallelism.
