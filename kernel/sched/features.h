8209ae9649f44f714dac5806f976ef51e732237b
Danny Lin <danny@kdrag0n.dev>
2019-08-24 00:24:10 -0700

    sched: Disable strict find_best_target logic
    Very detrimental to jitter and overall UX.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -117,7 +117,7 @@ SCHED_FEAT(ENERGY_AWARE, false)
  */
 SCHED_FEAT(EAS_PREFER_IDLE, true)
 SCHED_FEAT(FIND_BEST_TARGET, true)
-SCHED_FEAT(FBT_STRICT_ORDER, true)
+SCHED_FEAT(FBT_STRICT_ORDER, false)
 
 /*
  * Apply schedtune boost hold to tasks of all sched classes.
