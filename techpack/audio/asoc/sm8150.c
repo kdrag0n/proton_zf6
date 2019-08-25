f6e51f9fbebd6a94994bfbed9a5e70694a6b3b9f
Danny Lin <danny@kdrag0n.dev>
2019-08-24 21:59:34 -0700

    ASoC: sm8150: Fix Asus "Quaternary MI2S_TX Hostless" link index
    Asus hard-coded the index of this link in userspace, so changing its
    index by adding or removing elements in front of it causes audio
    playback issues when the device's built-in speakers are being used. This
    manifests as a loss of depth and reduced volume.
    
    Move the link back to its original index and in front of the new haptics
    links to fix the issue.
    
    Test: audio clarity and volume is on par with the stock kernel
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- techpack/audio/asoc/sm8150.c
+++ techpack/audio/asoc/sm8150.c
@@ -5837,6 +5837,25 @@ static struct snd_soc_dai_link msm_common_misc_fe_dai_links[] = {
 		.ignore_pmdown_time = 1,
 		.id = MSM_FRONTEND_DAI_MULTIMEDIA17,
 	},
+	{/* hw:x,42 */
+		.name = "Quaternary MI2S_TX Hostless",
+		.stream_name = "Quaternary MI2S_TX Hostless",
+		.cpu_dai_name = "QUAT_MI2S_TX_HOSTLESS",
+		.platform_name	= "msm-pcm-hostless",
+		.dynamic = 1,
+		.dpcm_capture = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		 /* this dailink has playback support */
+		.ignore_pmdown_time = 1,
+		/* This dainlink has MI2S support */
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+		// .codec_name = "tavil_codec",
+		// .codec_dai_name = "tavil_vifeedback",
+	},
 	{
 		.name = "MultiMedia30 Playback",
 		.stream_name = "MultiMedia30",
@@ -5866,25 +5885,6 @@ static struct snd_soc_dai_link msm_common_misc_fe_dai_links[] = {
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1,
 		.id = MSM_FRONTEND_DAI_MULTIMEDIA31,
-	},
-	{/* hw:x,42 */
-		.name = "Quaternary MI2S_TX Hostless",
-		.stream_name = "Quaternary MI2S_TX Hostless",
-		.cpu_dai_name = "QUAT_MI2S_TX_HOSTLESS",
-		.platform_name	= "msm-pcm-hostless",
-		.dynamic = 1,
-		.dpcm_capture = 1,
-		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
-			SND_SOC_DPCM_TRIGGER_POST},
-		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
-		.ignore_suspend = 1,
-		 /* this dailink has playback support */
-		.ignore_pmdown_time = 1,
-		/* This dainlink has MI2S support */
-		.codec_dai_name = "snd-soc-dummy-dai",
-		.codec_name = "snd-soc-dummy",
-		// .codec_name = "tavil_codec",
-		// .codec_dai_name = "tavil_vifeedback",
 	},
 };
 
