bc1052b7271ef18485711dd2c2900e1c04e9a17e
Danny Lin <danny@kdrag0n.dev>
2020-03-22 18:25:33 -0700

    Revert "cpufreq: stats: Drop spinlock in favor of atomic operations"
    This reverts commit 856c28eb6d68d83dbfbe79824a98c2b791b02f4e.
    
    Google has a "proper" version of this in the Pixel 4 kernel.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -14,13 +14,15 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
+static DEFINE_SPINLOCK(cpufreq_stats_lock);
+
 struct cpufreq_stats {
 	unsigned int total_trans;
 	unsigned long long last_time;
 	unsigned int max_state;
 	unsigned int state_num;
 	unsigned int last_index;
-	atomic64_t *time_in_state;
+	u64 *time_in_state;
 	unsigned int *freq_table;
 	unsigned int *trans_table;
 };
@@ -28,10 +30,12 @@ struct cpufreq_stats {
 static int cpufreq_stats_update(struct cpufreq_stats *stats)
 {
 	unsigned long long cur_time = get_jiffies_64();
+	unsigned long flags;
 
-	atomic64_add(cur_time - stats->last_time,
-		     &stats->time_in_state[stats->last_index]);
-	WRITE_ONCE(stats->last_time, cur_time);
+	spin_lock_irqsave(&cpufreq_stats_lock, flags);
+	stats->time_in_state[stats->last_index] += cur_time - stats->last_time;
+	stats->last_time = cur_time;
+	spin_unlock_irqrestore(&cpufreq_stats_lock, flags);
 	return 0;
 }
 
@@ -39,9 +43,9 @@ static void cpufreq_stats_clear_table(struct cpufreq_stats *stats)
 {
 	unsigned int count = stats->max_state;
 
-	memset(stats->time_in_state, 0, count * sizeof(atomic64_t));
+	memset(stats->time_in_state, 0, count * sizeof(u64));
 	memset(stats->trans_table, 0, count * count * sizeof(int));
-	WRITE_ONCE(stats->last_time, get_jiffies_64());
+	stats->last_time = get_jiffies_64();
 	stats->total_trans = 0;
 }
 
@@ -58,11 +62,9 @@ static ssize_t show_time_in_state(struct cpufreq_policy *policy, char *buf)
 
 	cpufreq_stats_update(stats);
 	for (i = 0; i < stats->state_num; i++) {
-		u64 time_in_state = atomic64_read(&stats->time_in_state[i]);
-
 		len += sprintf(buf + len, "%u %llu\n", stats->freq_table[i],
 			(unsigned long long)
-			jiffies_64_to_clock_t(time_in_state));
+			jiffies_64_to_clock_t(stats->time_in_state[i]));
 	}
 	return len;
 }
@@ -177,7 +179,7 @@ void cpufreq_stats_create_table(struct cpufreq_policy *policy)
 	if (!stats)
 		return;
 
-	alloc_size = count * sizeof(int) + count * sizeof(atomic64_t);
+	alloc_size = count * sizeof(int) + count * sizeof(u64);
 
 	alloc_size += count * count * sizeof(int);
 
