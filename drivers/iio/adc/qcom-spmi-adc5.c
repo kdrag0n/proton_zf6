7e875ab997c62a20dbf5a360a6556cf5f1e1fe3b
Danny Lin <danny@kdrag0n.dev>
2020-08-08 19:19:27 -0700

    iio: spmi-adc5: Add GPIO4 channel for Asus charger
    This is required for the charging driver to detect the Asus stock
    charger correctly.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/iio/adc/qcom-spmi-adc5.c
+++ drivers/iio/adc/qcom-spmi-adc5.c
@@ -771,6 +771,8 @@ static const struct adc_channels adc_chans_pmic5[ADC_MAX_CHANNEL] = {
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
 	[ADC_GPIO4_PU2]	= ADC_CHAN_TEMP("gpio4_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+        [ADC_GPIO4]	= ADC_CHAN_VOLT("gpio4", 1,
+					SCALE_HW_CALIB_DEFAULT)
 };
 
 static const struct adc_channels adc_chans_rev2[ADC_MAX_CHANNEL] = {
